f5462feaf0216f1252cbee963e7a4c6e4c175276
Sehwan Noh <devnoh@gmail.com>
2017-03-27 23:48:09 -0700

    Initial commit

---
+++ src/main/java/devnoh/demoapp/kafka/consumer/Receiver.java
@@ -0,0 +1,25 @@
+package devnoh.demoapp.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Receiver {
+
+    private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @KafkaListener(topics = "helloworld.t")
+    public void receiveMessage(String message) {
+        logger.info("received message='{}'", message);
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+}
---
+++ src/test/java/devnoh/demoapp/ApplicationTests.java
@@ -0,0 +1,37 @@
+package devnoh.demoapp;
+
+import devnoh.demoapp.kafka.consumer.Receiver;
+import devnoh.demoapp.kafka.producer.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Autowired
+    private Sender sender;
+
+    @Autowired
+    private Receiver receiver;
+
+//	@Test
+//	public void contextLoads() {
+//	}
+
+    @Test
+    public void testReceiver() throws Exception {
+        sender.sendMessage("helloworld.t", "Hello Spring Kafka!");
+
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+    }
+
+}
